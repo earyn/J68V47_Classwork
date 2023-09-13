72c847b13ee2afecd9cc056cc972fbcd5ea5b564
30075979 <30075979@nescol.ac.uk>
2023-09-13 15:47:17 +0100

    .

--- src/L3E2.java
+++ src/L3E2.java
@@ -7,6 +7,11 @@ public class L3E2 {
         System.out.println("What is the capital of Spain? ");
         String ansOne = input.nextLine();
         System.out.println(ansOne.toUpperCase().contains("MADRID"));
-
+        System.out.println("What is the largest big cat? ");
+        String ansTwo = input.nextLine();
+        System.out.println(ansTwo.toUpperCase().contains("TIGER"));
+        System.out.println("How many bones are there typically in the adult human body? ");
+        String ansThree= input.nextLine();
+        System.out.println(ansThree.toUpperCase().contains("206"));
     }
 }
