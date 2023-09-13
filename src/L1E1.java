c173aa585207d3b085c69434b8919af67b969032
Earyn <30075979@nescol.ac.uk>
2023-09-13 22:08:11 +0100

    .

---
+++ src/L1E1.java
@@ -0,0 +1,12 @@
+public class L1E1 {
+    public static void main(String[] args) {
+
+        System.out.println("Here are five 10-letter anagrams");
+        System.out.format("PERCUSSION = supersonic%n");
+        System.out.println("PERSISTENT = prettiness");
+        System.out.println("COORDINATE = decoration");
+        System.out.print("DICTIONARY = indicatory\n");
+        System.out.println("INDISCREET = iridescent");
+    }
+
+}
--- src/L2E3.java
+++ src/L2E3.java
@@ -2,27 +2,34 @@ import java.util.*;
 
 public class L2E3 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        Scanner scanner3 = new Scanner(System.in);
+
         System.out.println("What is the first item? ");
-        String firstItem = input.nextLine();
+        String firstItem = scanner.nextLine();
         System.out.println("How much did the " + firstItem + " cost? ");
-        int item1 = input.nextInt();
-        input.nextLine();
+        float item1 = scanner.nextFloat();
+
 
         System.out.println("What is the second item? ");
-        String secondItem = input.nextLine();
+        String secondItem = scanner2.nextLine();
         System.out.println("How much did the " + secondItem + " cost? ");
-        int item2 = input.nextInt();
-        input.nextLine();
+        float item2 = scanner.nextFloat();
+
 
         System.out.println("What is the third item? ");
-        String thirdItem = input.nextLine();
+        String thirdItem = scanner3.nextLine();
         System.out.println("How much did the " + thirdItem + " cost? ");
-        int item3 = input.nextInt();
-        input.nextLine();
+        float item3 = scanner3.nextFloat();
 
-        System.out.format(firstItem + item1);
-        System.out.println(secondItem + item2);
-        System.out.println(thirdItem + item3);
+
+        float subtotal = item1 + item2 + item3;
+
+        System.out.println("RECEIPT");
+        System.out.format("%s........£%.2f %n",firstItem, item1);
+        System.out.format("%s........£%.2f %n",secondItem, item2);
+        System.out.format("%s........£%.2f %n",thirdItem, item3);
+        System.out.format("SUBTOTAL..£%.2f %n",subtotal);
     }
 }
